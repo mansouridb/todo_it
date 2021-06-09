e66379145dbcaf500ec372b3ce5fb7273af45dd4
Majid <Mansouridb@gmail.com>
2021-06-10 00:12:17 +0200

    Run task 7 Create a new class TodoSequencer in data package that have the same behaviour as PersonSequencer but different method names.

---
+++ src/main/java/org/example/data/TodoSequencer.java
@@ -0,0 +1,15 @@
+package org.example.data;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class TodoSequencer {
+    private static int  personId;
+    private TodoSequencer(){
+
+    }
+    private void resetTodo(){
+        Set personId= Collections.singleton(0);
+
+    }
+}
