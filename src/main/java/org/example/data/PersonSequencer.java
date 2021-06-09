366325db2d060f342498db81eecdd0394f800fc5
Majid <Mansouridb@gmail.com>
2021-06-09 23:09:02 +0200

    Run tasks 5.Create new package called data. 6. Create a new class PersonSequencer in data package

---
+++ src/main/java/org/example/data/PersonSequencer.java
@@ -0,0 +1,16 @@
+package org.example.data;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class PersonSequencer<privat> {
+    private static int personId;
+    privat nextPersonId(){
+
+        return null;
+    }
+    private void reset(){
+        Set personId= Collections.singleton(0);
+
+    }
+}
---
+++ src/test/java/org/example/data/PersonSequencerTest.java
@@ -0,0 +1,7 @@
+package org.example.data;
+
+import junit.framework.TestCase;
+
+public class PersonSequencerTest extends TestCase {
+
+}
